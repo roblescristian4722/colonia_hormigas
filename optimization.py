e515a1966e0c1a90fd20442ca47ca7000c1f9e98
roblescristian4722 <roblescristian4722@gmail.com>
2021-09-28 18:23:11 -0500

    Ant selection has improved

--- optimization.py
+++ optimization.py
@@ -12,33 +12,25 @@ def absolute(*args) -> float:
 
 def get_intervals(dom: list, dimensions: int, poblationSize: int, tao: float):
     newDom = []
-    newDomTao = []
     tmp = []
-    tmpTao = []
     for i in range(1, len(dom) + 1):
-        tmp.append(dom[i - 1])
-        tmpTao.append(tao)
+        tmp.append([dom[i - 1], tao])
         if i % (poblationSize / dimensions) == 0:
             newDom.append(tmp)
-            newDomTao.append(tmpTao)
             tmp = []
-            tmpTao = []
-    return newDom, newDomTao
+    return newDom
 
-def generate_ants(dom: list, tao: list):
+def generate_ants(dom: list):
     ants = []
-    taoAnts = []
     for i in range(len(dom)):
         rand = int((random() * 100) % len(dom[i]))
-        ants.append(dom[i][rand])
-        taoAnts.append(tao[i][rand])
-    return ants, taoAnts
+        ants.append([dom[i][rand][0], dom[i][rand][1]])
+    return ants
 
 def inner_sum(x: list):
     sum = 0
-    for i in x:
-        for j in i:
-            sum += j
+    for _, v in x:
+        sum += v
     return sum
 
 def greatest(x: list):
@@ -49,11 +41,11 @@ def greatest(x: list):
     return gr
 
 # Datos iniciales
-generations = 500
+generations = 200
 domStart = -10
 domEnd = 10
-poblationSize = 200
-dimensions = 10
+poblationSize = 400
+dimensions = 50
 # Tasa de evaportación
 ro = 0.01
 increment = (abs(domStart) + abs(domEnd)) / (poblationSize - 1)
@@ -62,37 +54,64 @@ tao = 0.1
 Q = 1
 
 domain = [ domStart + (increment * i) for i in range(poblationSize) ]
-domain, taoDom = get_intervals(domain, dimensions, poblationSize, tao)
+domain = get_intervals(domain, dimensions, poblationSize, tao)
 
-ants, taoAnts = generate_ants(domain, taoDom)
+ants = generate_ants(domain)
+best = 0
 
 for g in range(generations):
-
-    for i in range(len(taoDom)):
-        taoSum = sum(taoDom[i])
-        for j in range(len(taoDom[i])):
-            taoDom[i][j] /= taoSum
+    for i in range(len(domain)):
+        taoSum = inner_sum(domain[i])
+        for j in range(len(domain[i])):
+            domain[i][j][1] /= taoSum
         probStart = 0
         probRate = random()
-        for j in range(len(taoDom[i])):
-            probStart += taoDom[i][j]
+        if g <= 10:
+            domain[i].sort(key=lambda x: absolute(x[0]))
+        else:
+            domain[i].sort(key=lambda x: x[1])
+        for j in range(len(domain[i])):
+            probStart += domain[i][j][1]
             if probStart >= probRate:
                 ants[i] = domain[i][j]
-                taoAnts[i] = taoDom[i][j]
                 break
 
     for i in range(len(domain)):
         for j in range(len(domain[i])):
             if domain[i][j] in ants:
                 try:
-                    taoDom[i][j] = Q / absolute(domain[i][j])
+                    domain[i][j][1] = Q / (1 / absolute(domain[i][j][1]))
                 except ZeroDivisionError:
-                    taoDom[i][j] = Q / 0.0000000000000001
+                    domain[i][j][1] = Q / (1 / 0.0000000000000001)
             else:
-                taoDom[i][j] = 0
-        taoSum = sum(taoDom[i])
+                domain[i][j][1] = 0
+        taoSum = inner_sum(domain[i])
         for j in range(len(domain[i])):
-            taoDom[i][j] = ( (1 - ro) * taoDom[i][j] ) + taoSum
+            domain[i][j][1] = ( (1 - ro) * domain[i][j][1] ) + taoSum
+
+for i in range(len(ants)):
+    ants[i][1] = tao
+
+for g in range(generations):
+    taoSum = inner_sum(ants)
+    for i in range(len(ants)):
+        ants[i][1] /= taoSum
+    probStart = 0
+    probRate = random()
+    ants.sort(key=lambda x: x[1])
+    for i in range(len(ants)):
+        probStart += ants[i][1]
+        if probStart >= probRate:
+            ants[i][1] = Q / ( 1 / absolute(ants[i][0]) )
+        else:
+            ants[i][1] = 0
+        ants[i][1] = ( (1 - ro) * ants[i][1] ) + taoSum
+    ants.sort(key=lambda x: x[1])
+    best = ants[0][0]
+    print(f"best ({g}): {best}")
+    input()
+
+# print(domain, "\n")
 
 # res = [ [ ants[i], abs(prob[i]) ] for i in range(len(prob)) ]
 # res.sort(key=lambda x: x[1], reverse=True)
